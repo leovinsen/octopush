c721873335873e6ad96af6563a00e1e0f853e5f4
Leonardo Vinsen <leonardovinsen@gmail.com>
2020-01-26 22:45:10 +0700

    Closes #35 + removed glow scroll effect

--- lib/screens/installment_detail_page.dart
+++ lib/screens/installment_detail_page.dart
@@ -3,12 +3,14 @@ import 'package:octopush/model/installment.dart';
 import 'package:octopush/repository/installment_repository.dart';
 import 'package:octopush/styles.dart';
 import 'package:octopush/utils/currency_utils.dart';
+import 'package:octopush/utils/no_glow_scroll.dart';
 
 class InstallmentDetailPage extends StatelessWidget {
   final InstallmentType installmentType;
   final String label;
 
-  InstallmentDetailPage(this.installmentType, this.label) : repository = InstallmentRepository();
+  InstallmentDetailPage(this.installmentType, this.label)
+      : repository = InstallmentRepository();
 
   final InstallmentRepository repository;
 
@@ -22,43 +24,54 @@ class InstallmentDetailPage extends StatelessWidget {
   }
 
   Widget _buildBody(BuildContext context) {
-    List<InstallmentOption> options = repository.getOptions(this.installmentType);
-    var basePrice = CurrencyUtils.formatToIdr(basePrices[installmentType.index].toDouble());
+    List<InstallmentOption> options =
+        repository.getOptions(this.installmentType);
+    var basePrice =
+        CurrencyUtils.formatToIdr(basePrices[installmentType.index].toDouble());
 
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            '$label Options',
-            style: titleStyle,
-          ),
-          SizedBox(height: 10.0),
-          Text('Base price: $basePrice ', style: subtitleStyle, ),
-          SizedBox(height: 20.0),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: options.length,
-            itemBuilder: (_, index) {
+      child: ScrollConfiguration(
+        behavior: NoGlowScrollBehavior(),
+        child: ListView(
+          children: <Widget>[
+            Text(
+              '$label Options',
+              style: titleStyle,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Base price: $basePrice ',
+              textAlign: TextAlign.center,
+              style: subtitleStyle,
+            ),
+            SizedBox(height: 20.0),
+            ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: options.length,
+                itemBuilder: (_, index) {
+                  var option = options[index];
+                  var pricePerInterval =
+                      CurrencyUtils.formatToIdr(option.intervalInstallment);
+                  var duration = option.duration;
+                  var total = CurrencyUtils.formatToIdr(option.amount);
+                  var interest = option.interest;
 
-              var option = options[index];
-              var pricePerInterval = CurrencyUtils.formatToIdr(option.intervalInstallment);
-              var duration = option.duration;
-              var total = CurrencyUtils.formatToIdr(option.amount);
-              var interest = option.interest;
-              
-              return Card(
-                child: ListTile(
-                  isThreeLine: true,
-                  title: Text('$pricePerInterval per interval'),
-                  subtitle: Text('$duration intervals \nTotal: $total'),
-                  trailing: Text('$interest %', ),
-                ),
-              );
-
-          }),
-        ],
+                  return Card(
+                    child: ListTile(
+                      isThreeLine: true,
+                      title: Text('$pricePerInterval per interval'),
+                      subtitle: Text('$duration intervals \nTotal: $total'),
+                      trailing: Text(
+                        '$interest %',
+                      ),
+                    ),
+                  );
+                }),
+          ],
+        ),
       ),
     );
   }
