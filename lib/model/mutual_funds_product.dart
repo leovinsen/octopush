51891e6de6a12f84d1ddefdc456b6b2aec58ec64
Leonardo Vinsen <leonardovinsen@gmail.com>
2020-02-04 21:44:21 +0700

    Closes #71

--- lib/model/mutual_funds_product.dart
+++ lib/model/mutual_funds_product.dart
@@ -1,12 +1,14 @@
 class MutualFundsProduct {
   final String name;
   final MutualFundsType type;
+  final double yieldRate;
 
-  MutualFundsProduct(this.name, this.type);
+  MutualFundsProduct(this.name, this.type, this.yieldRate);
 
   MutualFundsProduct.fromMap(Map<String,dynamic> map):
     name = map['name'],
-    type = MutualFundsType.values[map['type']];
+    type = MutualFundsType.values[map['type']],
+    yieldRate = map['yield'];
 }
 
 enum MutualFundsType {
--- lib/screens/mutual_funds/mutual_funds_page.dart
+++ lib/screens/mutual_funds/mutual_funds_page.dart
@@ -2,32 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:octopush/model/mutual_funds.dart';
 import 'package:octopush/model/mutual_funds_product.dart';
 import 'package:octopush/styles.dart';
-import 'package:octopush/widgets/page_header.dart';
+import 'package:octopush/widgets/primary_container.dart';
 import 'package:octopush/widgets/safe_scaffold.dart';
 
 const List<Map<String, dynamic>> mutualFundsJson = [
   {
     "category_name": "BNP Paribas",
     "products": [
-      {"name": "Rupiah Plus IDR", "type": 0},
-      {"name": "Omega", "type": 1},
-      {"name": "Persona", "type": 2}
+      {"name": "Rupiah Plus IDR", "type": 0, "yield": 23.55},
+      {"name": "Omega", "type": 1, "yield": 50.65},
+      {"name": "Persona", "type": 2, "yield": 79.20}
     ]
   },
   {
     "category_name": "Schroder Dana",
     "products": [
-      {"name": "Likuid", "type": 0},
-      {"name": "Andalan II", "type": 1},
-      {"name": "Prestasi Plus", "type": 2}
+      {"name": "Likuid", "type": 0, "yield": 25.95},
+      {"name": "Andalan II", "type": 1, "yield": 46.20},
+      {"name": "Prestasi Plus", "type": 2, "yield": 83.80}
     ]
   },
   {
     "category_name": "FFS Principal",
     "products": [
-      {"name": "Cash Fund", "type": 0},
-      {"name": "Total Return Bond Fund", "type": 1},
-      {"name": "Islamic Equity Growth Syariah", "type": 2}
+      {"name": "Cash Fund", "type": 0, "yield": 32.70},
+      {"name": "Total Return Bond Fund", "type": 1, "yield": 41.40},
+      {"name": "Islamic Equity Growth Syariah", "type": 2, "yield": 80.10}
     ]
   }
 ];
@@ -44,17 +44,11 @@ class MutualFundsPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              PageHeader(
-                pageName: 'Mutual Funds',
-                headerIconUrl: 'assets/ic_mutual_funds.png',
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                'Options:',
-                style: titleStyleLight,
-              ),
+              _PageHeader(),
+              // PageHeader(
+              //   pageName: 'Mutual Funds',
+              //   headerIconUrl: 'assets/ic_mutual_funds.png',
+              // ),
               SizedBox(
                 height: 20.0,
               ),
@@ -150,11 +144,37 @@ class MutualFundsProductWidget extends StatelessWidget {
               style: baseStyle,
             ),
             Text(
-              '0%',
+              '${product.yieldRate}%',
               style: baseStyle,
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _PageHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PrimaryContainer(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'Options:',
+            style: titleStyleLight,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Image.asset(
+              'assets/ic_mutual_funds.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ],
       ),
     );
   }
