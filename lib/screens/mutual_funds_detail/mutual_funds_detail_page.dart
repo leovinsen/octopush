60dad62a3d908a62cb5edd33a1e28f8c5f813b6e
Leonardo Vinsen <leonardovinsen@gmail.com>
2020-02-10 22:30:57 +0700

    Closes #82

--- lib/screens/mutual_funds_detail/mutual_funds_detail_page.dart
+++ lib/screens/mutual_funds_detail/mutual_funds_detail_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:octopush/database/dao/mutual_funds_history_dao.dart';
+import 'package:octopush/database/database.dart';
 import 'package:octopush/model/mutual_funds_product.dart';
+import 'package:octopush/repository/mutual_funds_history_repository.dart';
+import 'package:octopush/repository/transaction_repository.dart';
 import 'package:octopush/screens/mutual_funds_detail/widgets/buy_sell_buttons.dart';
 import 'package:octopush/screens/mutual_funds_detail/widgets/performance_graph.dart';
+import 'package:octopush/service/interval_service.dart';
+import 'package:octopush/service/mutual_funds_service.dart';
+import 'package:octopush/service/transaction_service.dart';
 import 'package:octopush/service/trb_service.dart';
 import 'package:octopush/styles.dart';
 import 'package:octopush/widgets/primary_container.dart';
 import 'package:octopush/widgets/safe_scaffold.dart';
 import 'package:octopush/widgets/section_label.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'widgets/product_holdings.dart';
 import 'widgets/product_info.dart';
@@ -22,10 +30,6 @@ const List<List<String>> holdings = [
   ["Corporate Bonds"],
   ["Astra International", "Barito Pacific", "Indofood", "Telkom", "Unilever"]
 ];
-
-// const List<List<double>> fees = [
-//   []
-// ]
 
 class MutualFundsDetailPage extends StatelessWidget {
   final MutualFundsProduct product;
@@ -80,13 +84,81 @@ class MutualFundsDetailPage extends StatelessWidget {
                 height: 10.0,
               ),
               PerformanceGraph(),
-              SizedBox(height: 20.0,),
-              BuySellButtons(),
+              SizedBox(
+                height: 20.0,
+              ),
+              BuySellButtons(
+                onSellPressed: null,
+                onBuyPressed: () => _showBuyDialog(context),
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _showBuyDialog(BuildContext context) {
+    final controller = TextEditingController();
+    var formKey = GlobalKey<FormState>();
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        content: Form(
+          key: formKey,
+          child: TextFormField(
+            keyboardType: TextInputType.number,
+            validator: (text) {
+              int amount = int.parse(text);
+              if (amount <= 100000)
+                return 'You entered an amount less than 100K IDR!';
+
+              return null;
+            },
+            controller: controller,
+            decoration: InputDecoration(labelText: 'Amount (min 100K IDR)'),
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('NO'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text('YES'),
+            onPressed: () {
+              if (formKey.currentState.validate())
+                _buy(
+                  context,
+                  int.parse(controller.text),
+                );
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  void _buy(BuildContext context, int amount) async {
+    await createService()
+      ..buyMutualFunds(product, amount.toDouble());
+    Navigator.of(context).popUntil(ModalRoute.withName('/mutualFunds'));
+  }
+
+  Future<MutualFundsService> createService() async {
+    var mfRepo = MutualFundsHistoryRepository(
+        MutualFundsHistoryDao(await DatabaseProvider.instance.db));
+
+    var intervalService =
+        IntervalService(await SharedPreferences.getInstance());
+
+    var transactionService =
+        TransactionService(TransactionRepository(), TrbService());
+
+    MutualFundsService service =
+        MutualFundsService(mfRepo, intervalService, transactionService);
+
+    return service;
   }
 }
 
--- lib/screens/mutual_funds_detail/widgets/buy_sell_buttons.dart
+++ lib/screens/mutual_funds_detail/widgets/buy_sell_buttons.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'custom_button.dart';
 
 class BuySellButtons extends StatelessWidget {
+  final Function onBuyPressed;
+  final Function onSellPressed;
+
+  const BuySellButtons({Key key, this.onBuyPressed, this.onSellPressed}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -10,7 +15,7 @@ class BuySellButtons extends StatelessWidget {
         Expanded(
           child: CustomButton(
             color: Theme.of(context).accentColor,
-            onPressed: () => print('a'),
+            onPressed: () => onSellPressed(),
             text: 'SELL',
           ),
         ),
@@ -20,7 +25,7 @@ class BuySellButtons extends StatelessWidget {
         Expanded(
           child: CustomButton(
             color: Colors.green,
-            onPressed: () => print('a'),
+            onPressed: () => onBuyPressed(),
             text: 'BUY',
           ),
         ),
